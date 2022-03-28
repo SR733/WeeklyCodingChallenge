b2ca34ed15f16c20cd8fad1fe112ad00a1f3b65f
SR733 <57599866+SR733@users.noreply.github.com>
2022-03-28 19:00:59 -0400

    Update WeeklyCodingChallenge -Num of Lockers Open.js
    add context to my script

--- WeeklyCodingChallenge -Num of Lockers Open.js
+++ WeeklyCodingChallenge -Num of Lockers Open.js
@@ -1,7 +1,16 @@
 /*
 Pls provide the link to your solution in the comments section not later than Friday morning.
 
-100 Lockers: There are 100 closed lockers in a hallway. A man begins by opening all 100 lockers. Next, he closes every second locker. Then, on his third pass, he toggles every third locker (closes it if it is open or opens it if it is closed). This process continues for 100 passes, such that on each pass 'i', the man toggles every 'i'th locker. After his 100th pass in the hallway, in which he toggles only locker #100, how many lockers are open?
+100 Lockers: There are 100 closed lockers in a hallway. A man begins by opening all 100 lockers. 
+Next, he closes every second locker. Then, on his third pass, he toggles every third locker 
+(closes it if it is open or opens it if it is closed). This process continues for 100 passes, 
+such that on each pass 'i', the man toggles every 'i'th locker. After his 100th pass in the 
+hallway, in which he toggles only locker #100, how many lockers are open?
+*/
+/*
+Answer: The state(open or closed) of the lockers will be opened only if the number of factors
+is odd. The only lockers opened with the odd factors are the ones that are the perfect squares
+(1,4,9,16,25,36,49,64,81,and 100). 
 */
 
     var lockers = []; //number of lockers
